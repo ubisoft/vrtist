01ff49e6a351e1c160fd3eb5d8b0b7d6837afa3c
droune2001 <nfauvet@gmail.com>
2019-10-14 17:11:23 +0200

    Added a test scene for Assimp

---
+++ Assets/Sandbox/LoadFBX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assimp;
+
+public class LoadFBX : MonoBehaviour
+{
+    [SerializeField]
+    private int meshCount = 0;
+
+    [SerializeField]
+    private GameObject cube = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Assimp.AssimpContext ctx = new AssimpContext();
+        Assimp.Scene aScene = ctx.ImportFile("C:\\Texel_4M.FBX", Assimp.PostProcessSteps.None);
+        if (aScene.HasMeshes)
+        {
+            meshCount = aScene.MeshCount;
+            cube.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
