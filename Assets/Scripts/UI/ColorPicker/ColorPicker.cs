c25b8b02d625a95e434ab6abb809d06fa0d620e5
thomas.capelle <thomas.capelle@ubisoft.com>
2019-11-15 11:35:20 +0100

    Color Picker wip

---
+++ Assets/Scripts/UI/ColorPicker/ColorPicker.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRtist
+{
+    public class ColorPicker : MonoBehaviour
+    {
+        Color currentColor;
+        public Color CurrentColor
+        {
+            get { return currentColor; }
+            set { SetColor(value);  }
+        }
+
+        public Hue hue;
+        public Saturation saturation;
+        public GameObject preview;
+
+        void Start()
+        {
+            OnColorChanged();
+            CurrentColor = new Color(0.2f, 0.5f, 0.6f);
+        }
+
+        public void SetColor(Color color)
+        {
+            currentColor = color;
+            float h, s, v;
+            Color.RGBToHSV(color, out h, out s, out v);
+            hue.SetHue(h);
+
+            Color baseColor = Color.HSVToRGB(h, 1f, 1f);
+            saturation.SetBaseColor(baseColor);
+            saturation.SetSaturation(new Vector2(s, v));
+
+            SetPreviewColor(color);
+        }
+
+        public void OnColorChanged()
+        {
+            float h = hue.GetHue();
+            Vector2 sat = saturation.GetSaturation();
+
+            Color baseColor = Color.HSVToRGB(h, 1f, 1f);
+            currentColor = Color.Lerp(Color.white, baseColor, sat.x);
+            currentColor = Color.Lerp(Color.black, currentColor, sat.y);
+
+            SetPreviewColor(currentColor);
+            saturation.SetBaseColor(baseColor);
+        }
+
+        public void SetPreviewColor(Color color)
+        {
+            var renderer = preview.GetComponent<Renderer>();
+            renderer.material.SetColor("_Color", color);
+        }
+
+    }
+}
---
+++ Assets/Scripts/UI/ColorPicker/Saturation.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRtist
+{
+    public class Saturation : MonoBehaviour
+    {
+        ColorPicker colorPicker;
+        Color baseColor;
+        Vector2 cursorPosition = new Vector2(0.5f, 0.5f);
+
+        public Transform cursor;
+
+        void Awake()
+        {
+            colorPicker = GetComponentInParent<ColorPicker>();
+        }
+
+        public void SetBaseColor(Color clr)
+        {
+            baseColor = clr;
+            var renderer = GetComponent<Renderer>();
+            renderer.material.SetColor("_Color", clr);
+        }
+
+        public Vector2 GetSaturation()
+        {
+            return cursorPosition;
+        }
+
+        public void SetSaturation(Vector2 sat)
+        {
+            cursorPosition = sat;
+            cursor.localPosition = new Vector3(sat.x - 0.5f, sat.y - 0.5f, cursor.localPosition.z);
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            Vector3 position = transform.worldToLocalMatrix.MultiplyPoint(other.transform.position);
+            float x = position.x + 1f * 0.5f;
+            float y = position.y + 1f * 0.5f;
+            x = Mathf.Clamp(x, 0, 1);
+            y = Mathf.Clamp(y, 0, 1);
+            SetSaturation( new Vector2(x, y) );
+
+            colorPicker.OnColorChanged();
+        }
+    }
+}
