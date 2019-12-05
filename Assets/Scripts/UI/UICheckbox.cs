c16d32eb034d8e96ff3a17cd04ce0fbf058b3150
droune2001 <nfauvet@gmail.com>
2019-12-05 11:03:34 +0100

    Fixed image on button and checkbox not updating size.

--- Assets/Scripts/UI/UICheckbox.cs
+++ Assets/Scripts/UI/UICheckbox.cs
@@ -89,14 +89,32 @@ namespace VRtist
                 RectTransform canvasRT = canvas.gameObject.GetComponent<RectTransform>();
                 canvasRT.sizeDelta = new Vector2(width, height);
 
+                float minSide = Mathf.Min(width, height);
+
+                // IMAGE
+                Image image = canvas.GetComponentInChildren<Image>();
+                if (image != null)
+                {
+                    RectTransform rt = image.gameObject.GetComponent<RectTransform>();
+                    if (rt)
+                    {
+                        rt.sizeDelta = new Vector2(minSide - 2.0f * margin, minSide - 2.0f * margin);
+                        rt.localPosition = new Vector3(margin, -margin, -0.001f);
+                    }
+                }
+
+                // TEXT
                 Text text = canvas.gameObject.GetComponentInChildren<Text>();
                 if (text != null)
                 {
-                    RectTransform textRT = text.gameObject.GetComponent<RectTransform>();
-                    textRT.sizeDelta = new Vector2(width, height);
+                    RectTransform rt = text.gameObject.GetComponent<RectTransform>();
+                    if (rt != null)
+                    {
+                        rt.sizeDelta = new Vector2(width, height);
+                        float textPosLeft = image != null ? minSide : 0.0f;
+                        rt.localPosition = new Vector3(textPosLeft, -height / 2.0f, -0.002f);
+                    }
                 }
-
-                // TODO: image
             }
         }
 
