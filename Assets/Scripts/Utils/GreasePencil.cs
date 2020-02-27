6315e0c0c0ebc5f28d05ec74b73d0ee3475b418c
thomas.capelle <thomas.capelle@ubisoft.com>
2020-02-27 12:13:57 +0100

    grease pencil message refacto

---
+++ Assets/Scripts/Utils/GreasePencil.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRtist
+{
+    public class GreasePencil : MonoBehaviour
+    {
+        public static int currentFrame = 1;
+
+        private Dictionary<int, Mesh> meshes;
+        private int frame = -1;
+
+        private Mesh findMesh(int frame)
+        {
+            int curFrame = -1;
+            foreach(int f in meshes.Keys)
+            {
+                if (f > frame)
+                    break;
+                curFrame = f;
+            }
+
+            return meshes[curFrame];
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (currentFrame == frame)
+                return;
+
+            frame = currentFrame;
+            MeshFilter meshFilter = GetComponent<MeshFilter>();
+            meshFilter.mesh = findMesh(frame);
+        }
+
+        public void AddMesh(int frame, Mesh mesh)
+        {
+            meshes[frame] = mesh;
+        }
+    }
+}
