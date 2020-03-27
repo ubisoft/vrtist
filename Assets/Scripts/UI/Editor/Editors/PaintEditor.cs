a4e84365e30918e9c27e2c2198f5e9e97eaf3b53
nfauvet <nfauvet@gmail.com>
2020-03-27 16:15:08 +0100

    Generate Brushstrokes for debug.

---
+++ Assets/Scripts/UI/Editor/Editors/PaintEditor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEditor;
+
+namespace VRtist
+{
+    [CustomEditor(typeof(Paint))]
+    public class PaintEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+        }
+
+        private void OnSceneGUI()
+        {
+            Paint paint = target as Paint;
+
+            Handles.BeginGUI();
+
+            GUILayout.BeginArea(new Rect(10, 10, Screen.width, Screen.height));
+            GUILayout.BeginVertical();
+            {
+                if (GUILayout.Button("Generate Random BrushStroke", GUILayout.Width(200), GUILayout.Height(30)))
+                {
+                    paint.GenerateRandomBrushStroke();
+                }
+            }
+            GUILayout.EndVertical();
+            GUILayout.EndArea();
+
+            Handles.EndGUI();
+        }
+    }
+}
