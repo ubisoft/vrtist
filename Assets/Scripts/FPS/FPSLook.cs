575d4b395a67b9b7a3052b1df9ff92b9386e7b5a
thomas <grosglob@free.fr>
2020-03-31 17:10:16 +0200

    FPS camera in COVID-19-Thomas

---
+++ Assets/Scripts/FPS/FPSLook.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSLook : MonoBehaviour
+{
+    public float sensitivity = 400;
+    public Transform playerBody;
+    float xRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float z = Input.GetAxis("Fire1");
+        if (z <= 0)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            return;
+        }
+
+        Cursor.lockState = CursorLockMode.Locked;            
+
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/FPS/FPSMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSMove : MonoBehaviour
+{
+    public Transform playerBody;
+    public float speed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * speed;
+        float y = Input.GetAxis("Vertical") * speed;
+
+        Vector3 move = transform.right * x + transform.forward * y;
+        playerBody.localPosition += move;
+    }
+}
