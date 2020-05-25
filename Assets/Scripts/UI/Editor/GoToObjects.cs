f4969e6d60381dadc1f9cc649a0750db2ff1cd8a
nfauvet <nfauvet@gmail.com>
2020-05-25 11:56:14 +0200

    Added shortcut actions to go to Palette, Panels and Tools in the hierarchy view.

---
+++ Assets/Scripts/UI/Editor/GoToObjects.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace VRtist
+{
+    public class GoToUtils
+    {
+        [MenuItem("VRtist/GOTO Palette")]
+        static void GoToPalette()
+        {
+            GameObject palette = GameObject.Find("Camera Rig/Pivot/LeftHandle/PaletteHandle/Palette");
+            UnityEditor.Selection.activeTransform = palette.transform;
+        }
+
+        [MenuItem("VRtist/GOTO Panels")]
+        static void GoToPanels()
+        {
+            GameObject panels = GameObject.Find("Camera Rig/Pivot/LeftHandle/PaletteHandle/Palette/MainPanel/ToolsPanelGroup");
+            UnityEditor.Selection.activeTransform = panels.transform;
+        }
+
+        [MenuItem("VRtist/GOTO Tools")]
+        static void GoToTools()
+        {
+            GameObject tools = GameObject.Find("Camera Rig/Pivot/RightHandle/Tools");
+            UnityEditor.Selection.activeTransform = tools.transform;
+        }
+    }
+}
