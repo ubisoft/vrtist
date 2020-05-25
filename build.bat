07993ef09e774f8cbc514ae381e8a0c83988b7b5
ewik <ewik@free.fr>
2020-05-25 14:33:35 +0200

    script for command line build

---
+++ build.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+REM Build Unity from a builder script
+"E:\Programs\Unity\2019.3.5f1\Editor\Unity.exe" -quit -batchmode -projectPath %~dp0 -executeMethod VRtist.Builder.PerformBuild -logFile "Build/build.log"
+
+REM Build unity from the default build settings
+REM "E:\Programs\Unity\2019.3.5f1\Editor\Unity.exe" -quit -batchmode -buildWindows64Player "Build/VRtist.exe" -logFile "Build/build.log"
