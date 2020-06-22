7dbc05fd843aa458875f39bd4ad9aa5a6540d149
nfauvet <nfauvet@gmail.com>
2020-06-22 16:45:47 +0200

    Clean usings

--- Assets/Scripts/UI/UITimeBar.cs
+++ Assets/Scripts/UI/UITimeBar.cs
@@ -1,10 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEditor;
-using UnityEngine.UI;
-using System;
 
 namespace VRtist
 {
