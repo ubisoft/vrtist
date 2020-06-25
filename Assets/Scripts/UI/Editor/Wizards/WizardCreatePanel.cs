9431425ab057dc047336f38841cc5731cfb43805
nfauvet <nfauvet@gmail.com>
2020-06-25 10:24:32 +0200

    UIPanel has an option to have a flat filled background.

--- Assets/Scripts/UI/Editor/Wizards/WizardCreatePanel.cs
+++ Assets/Scripts/UI/Editor/Wizards/WizardCreatePanel.cs
@@ -1,32 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEditor;
 using UnityEngine;
-using UnityEditor;
 
 namespace VRtist
 {
 
     public class WizardCreatePanel : ScriptableWizard
     {
-        public UIPanel parentPanel = null;
-        public string panelName = "Panel";
-        public float width = 0.4f;
-        public float height = 0.6f;
-        public float margin = 0.02f;
-        public float radius = 0.01f;
-        public Material uiMaterial = null;
-        public Color color = UIElement.default_background_color;
-
         private static readonly float default_width = 0.4f;
         private static readonly float default_height = 0.6f;
         private static readonly float default_margin = 0.02f;
         private static readonly float default_radius = 0.01f;
+        private static readonly float default_thickness = 0.001f;
+        private static readonly UIPanel.BackgroundGeometryStyle default_bg_geom_style = UIPanel.BackgroundGeometryStyle.Tube;
 
-        [MenuItem("VRtist/Create UI Panel")]
-        static void CreateWizard()
-        {
-            ScriptableWizard.DisplayWizard<WizardCreatePanel>("Create UI Panel", "Create");
-        }
+        public UIPanel parentPanel = null;
+        public string panelName = "New Panel";
+        public float width = default_width;
+        public float height = default_height;
+        public float margin = default_margin;
+        public float radius = default_radius;
+        public float thickness = default_thickness;
+        public UIPanel.BackgroundGeometryStyle backgroundGeometryStyle = default_bg_geom_style;
+        public Material uiMaterial = null;
+        public Color color = UIElement.default_background_color;
 
         [MenuItem("GameObject/VRtist/UIPanel", false, 49)]
         public static void OnCreateFromHierarchy()
@@ -38,22 +34,7 @@ namespace VRtist
                 parent = T;
             }
 
-            UIPanel.CreateUIPanel("Panel", parent, Vector3.zero, default_width, default_height, default_margin, default_radius, UIUtils.LoadMaterial("UIPanel"), UIElement.default_color);
-        }
-
-        private void OnWizardUpdate()
-        {
-            helpString = "Create a new UIPanel";
-
-            if (uiMaterial == null)
-            {
-                uiMaterial = UIUtils.LoadMaterial("UIPanel");
-            }
-        }
-
-        private void OnWizardCreate()
-        {
-            UIPanel.CreateUIPanel(panelName, parentPanel ? parentPanel.transform : null, Vector3.zero, width, height, margin, radius, uiMaterial, color);
+            UIPanel.Create("New Panel", parent, Vector3.zero, default_width, default_height, default_margin, default_radius,default_thickness, default_bg_geom_style, UIUtils.LoadMaterial("UIPanel"), UIElement.default_background_color);
         }
     }
 }
--- Assets/Scripts/UI/UIUtils.cs
+++ Assets/Scripts/UI/UIUtils.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
@@ -1190,6 +1189,314 @@ namespace VRtist
             return mesh;
         }
 
+        public static Mesh BuildRoundedRectTube(float width, float height, float margin, float radius)
+        {
+            const int default_circleSubdiv = 8;
+            const int default_nbSubdivPerUnit = 1;
+            const int default_nbSubdivCornerFixed = 3;
+            const int default_nbSubdivCornerPerUnit = 3;
+
+            return BuildRoundedRectTubeEx(
+                width, height, margin, radius,
+                default_circleSubdiv,
+                default_nbSubdivPerUnit,
+                default_nbSubdivCornerFixed,
+                default_nbSubdivCornerPerUnit
+            );
+        }
+
+        public static Mesh BuildRoundedRectTubeEx(float width, float height, float margin, float radius, int circleSubdiv, int nbSubdivPerUnit, int nbSubdivCornerFixed, int nbSubdivCornerPerUnit)
+        {
+            List<Vector3> vertices = new List<Vector3>(); // TODO: use Vector3[] vertices = new Vector3[computed_size];
+            List<Vector3> normals = new List<Vector3>(); // TODO: use Vector3[] normals = new Vector3[computed_size];
+            List<int> indices = new List<int>(); // TODO: use int[] indices = new int[computed_size];
+
+            float hTubeWidth = width - 2.0f * margin;
+            float vTubeHeight = height - 2.0f * margin;
+            float cornerLength = margin * Mathf.PI / 2.0f;
+            int nbSubdivOnWidth = Mathf.FloorToInt(hTubeWidth * nbSubdivPerUnit);
+            int nbSectionsOnWidth = 1 + nbSubdivOnWidth;
+            int nbCirclesOnWidth = 2 + nbSubdivOnWidth;
+            int nbSubdivOnHeight = Mathf.FloorToInt(vTubeHeight * nbSubdivPerUnit);
+            int nbSectionsOnHeight = 1 + nbSubdivOnHeight;
+            int nbCirclesOnHeight = 2 + nbSubdivOnHeight;
+            int nbSubdivOnCorner = Mathf.FloorToInt(nbSubdivCornerFixed + cornerLength * nbSubdivCornerPerUnit);
+            int nbSectionsOnCorner = 1 + nbSubdivOnCorner;
+            int nbCirclesOnCorner = 2 + nbSubdivOnCorner;
+            float sectionWidth = hTubeWidth / (float)(nbSubdivOnWidth + 1);
+            float sectionHeight = vTubeHeight / (float)(nbSubdivOnHeight + 1);
+
+            Vector3 panelTopLeft = new Vector3(0.0f, 0.0f, 0.0f);
+            Vector3 panelTopRight = new Vector3(width, 0.0f, 0.0f);
+            Vector3 panelBottomLeft = new Vector3(0.0f, -height, 0.0f);
+            Vector3 panelBottomRight = new Vector3(width, -height, 0.0f);
+
+            int indexOffset = 0;
+
+            #region top-horizontal-bar: left to right, circle right axis = +Z
+            {
+                Vector3 sectionOffset = panelTopLeft + new Vector3(margin, 0, 0);
+                for (int cs = 0; cs < nbCirclesOnWidth; ++cs)
+                {
+                    for (int i = 0; i < circleSubdiv; ++i)
+                    {
+                        float fi = (float)i / (float)circleSubdiv;
+                        vertices.Add(new Vector3(
+                            sectionOffset.x,
+                            sectionOffset.y + radius * Mathf.Sin(2.0f * Mathf.PI * fi),
+                            sectionOffset.z + radius * Mathf.Cos(2.0f * Mathf.PI * fi)
+                        ));
+                        normals.Add(new Vector3(
+                            0,
+                            Mathf.Sin(2.0f * Mathf.PI * fi),
+                            Mathf.Cos(2.0f * Mathf.PI * fi)
+                        ));
+                    }
+                    sectionOffset.x += sectionWidth;
+                }
+                FillCircleSectionsWithTriangles(indices, indexOffset, circleSubdiv, nbSectionsOnWidth);
+                indexOffset += nbCirclesOnWidth * circleSubdiv;
+            }
+            #endregion
+
+            #region bottom-horizontal-bar: left to right, circle right axis = +Z
+            {
+                Vector3 sectionOffset = panelBottomLeft + new Vector3(margin, 0, 0);
+                for (int cs = 0; cs < nbCirclesOnWidth; ++cs)
+                {
+                    for (int i = 0; i < circleSubdiv; ++i)
+                    {
+                        float fi = (float)i / (float)circleSubdiv;
+                        vertices.Add(new Vector3(
+                            sectionOffset.x,
+                            sectionOffset.y + radius * Mathf.Sin(2.0f * Mathf.PI * fi),
+                            sectionOffset.z + radius * Mathf.Cos(2.0f * Mathf.PI * fi)
+                        ));
+                        normals.Add(new Vector3(
+                            0,
+                            Mathf.Sin(2.0f * Mathf.PI * fi),
+                            Mathf.Cos(2.0f * Mathf.PI * fi)
+                        ));
+                    }
+                    sectionOffset.x += sectionWidth;
+                }
+                FillCircleSectionsWithTriangles(indices, indexOffset, circleSubdiv, nbSectionsOnWidth);
+                indexOffset += nbCirclesOnWidth * circleSubdiv;
+            }
+            #endregion
+
+            #region left-vertical-bar: bottom to top, circle right axis = +Z
+            {
+                Vector3 sectionOffset = panelBottomLeft + new Vector3(0, margin, 0);
+                for (int cs = 0; cs < nbCirclesOnHeight; ++cs)
+                {
+                    for (int i = 0; i < circleSubdiv; ++i)
+                    {
+                        float fi = (float)i / (float)circleSubdiv;
+                        vertices.Add(new Vector3(
+                            sectionOffset.x - radius * Mathf.Sin(2.0f * Mathf.PI * fi),
+                            sectionOffset.y,
+                            sectionOffset.z + radius * Mathf.Cos(2.0f * Mathf.PI * fi)
+                        ));
+                        normals.Add(new Vector3(
+                            -Mathf.Sin(2.0f * Mathf.PI * fi),
+                            0,
+                            Mathf.Cos(2.0f * Mathf.PI * fi)
+                        ));
+                    }
+                    sectionOffset.y += sectionHeight;
+                }
+                FillCircleSectionsWithTriangles(indices, indexOffset, circleSubdiv, nbSectionsOnHeight);
+                indexOffset += nbCirclesOnHeight * circleSubdiv;
+            }
+            #endregion
+
+            #region right-vertical-bar: bottom to top, circle right axis = +Z
+            {
+                Vector3 sectionOffset = panelBottomRight + new Vector3(0, margin, 0);
+                for (int cs = 0; cs < nbCirclesOnHeight; ++cs)
+                {
+                    for (int i = 0; i < circleSubdiv; ++i)
+                    {
+                        float fi = (float)i / (float)circleSubdiv;
+                        vertices.Add(new Vector3(
+                            sectionOffset.x - radius * Mathf.Sin(2.0f * Mathf.PI * fi),
+                            sectionOffset.y,
+                            sectionOffset.z + radius * Mathf.Cos(2.0f * Mathf.PI * fi)
+                        ));
+                        normals.Add(new Vector3(
+                            -Mathf.Sin(2.0f * Mathf.PI * fi),
+                            0,
+                            Mathf.Cos(2.0f * Mathf.PI * fi)
+                        ));
+                    }
+                    sectionOffset.y += sectionHeight;
+                }
+                FillCircleSectionsWithTriangles(indices, indexOffset, circleSubdiv, nbSectionsOnHeight);
+                indexOffset += nbCirclesOnHeight * circleSubdiv;
+            }
+            #endregion
+
+            #region top-left-corner: outer-circle from -X(left) to Y(top), circle right axis = +Z
+            {
+                Vector3 cornerCenter = panelTopLeft + new Vector3(margin, -margin, 0);
+                Vector3 unitRight = new Vector3(0, 0, 1);
+                Vector3 unitUp = new Vector3(0, 1, 0);
+                for (int cs = 0; cs < nbCirclesOnCorner; ++cs)
+                {
+                    float fQuarterPercent = 0.25f * (float)cs / (float)(nbCirclesOnCorner - 1); // [0..1], from top to left of corner quarter of circle.
+                    Vector3 outerCirclePos = new Vector3(
+                        -margin * Mathf.Cos(2.0f * Mathf.PI * fQuarterPercent),
+                        +margin * Mathf.Sin(2.0f * Mathf.PI * fQuarterPercent),
+                        0);
+                    Vector3 innerCircleCenter = cornerCenter + outerCirclePos;
+                    unitUp = outerCirclePos.normalized;
+                    for (int i = 0; i < circleSubdiv; ++i)
+                    {
+                        float fi = (float)i / (float)circleSubdiv;
+
+                        Vector3 innerCirclePos = unitRight * radius * Mathf.Cos(2.0f * Mathf.PI * fi)
+                                                + unitUp * radius * Mathf.Sin(2.0f * Mathf.PI * fi);
+                        vertices.Add(innerCircleCenter + innerCirclePos);
+
+                        normals.Add(innerCirclePos.normalized);
+                    }
+                }
+                FillCircleSectionsWithTriangles(indices, indexOffset, circleSubdiv, nbSectionsOnCorner);
+                indexOffset += nbCirclesOnCorner * circleSubdiv;
+            }
+            #endregion
+
+            #region top-right-corner: outer-circle from Y(top) to X(right), circle right axis = +Z
+            {
+                Vector3 cornerCenter = panelTopRight + new Vector3(-margin, -margin, 0);
+                Vector3 unitRight = new Vector3(0, 0, 1);
+                Vector3 unitUp = new Vector3(0, 1, 0);
+                for (int cs = 0; cs < nbCirclesOnCorner; ++cs)
+                {
+                    float fQuarterPercent = 0.25f * (float)cs / (float)(nbCirclesOnCorner - 1); // [0..1], from top to left of corner quarter of circle.
+                    Vector3 outerCirclePos = new Vector3(
+                        +margin * Mathf.Sin(2.0f * Mathf.PI * fQuarterPercent),
+                        +margin * Mathf.Cos(2.0f * Mathf.PI * fQuarterPercent),
+                        0);
+                    Vector3 innerCircleCenter = cornerCenter + outerCirclePos;
+                    unitUp = outerCirclePos.normalized;
+                    for (int i = 0; i < circleSubdiv; ++i)
+                    {
+                        float fi = (float)i / (float)circleSubdiv;
+
+                        Vector3 innerCirclePos = unitRight * radius * Mathf.Cos(2.0f * Mathf.PI * fi)
+                                                + unitUp * radius * Mathf.Sin(2.0f * Mathf.PI * fi);
+                        vertices.Add(innerCircleCenter + innerCirclePos);
+
+                        normals.Add(innerCirclePos.normalized);
+                    }
+                }
+                FillCircleSectionsWithTriangles(indices, indexOffset, circleSubdiv, nbSectionsOnCorner);
+                indexOffset += nbCirclesOnCorner * circleSubdiv;
+            }
+            #endregion
+
+            #region bottom-right-corner: outer-circle from -Y(bottom) to X(right), circle right axis = +Z
+            {
+                Vector3 cornerCenter = panelBottomRight + new Vector3(-margin, +margin, 0);
+                Vector3 unitRight = new Vector3(0, 0, 1);
+                Vector3 unitUp = new Vector3(0, 1, 0);
+                for (int cs = 0; cs < nbCirclesOnCorner; ++cs)
+                {
+                    float fQuarterPercent = 0.25f * (float)cs / (float)(nbCirclesOnCorner - 1); // [0..1], from top to left of corner quarter of circle.
+                    Vector3 outerCirclePos = new Vector3(
+                        +margin * Mathf.Sin(2.0f * Mathf.PI * fQuarterPercent),
+                        -margin * Mathf.Cos(2.0f * Mathf.PI * fQuarterPercent),
+                        0);
+                    Vector3 innerCircleCenter = cornerCenter + outerCirclePos;
+                    unitUp = -outerCirclePos.normalized;
+                    for (int i = 0; i < circleSubdiv; ++i)
+                    {
+                        float fi = (float)i / (float)circleSubdiv;
+
+                        Vector3 innerCirclePos = unitRight * radius * Mathf.Cos(2.0f * Mathf.PI * fi)
+                                                + unitUp * radius * Mathf.Sin(2.0f * Mathf.PI * fi);
+                        vertices.Add(innerCircleCenter + innerCirclePos);
+
+                        normals.Add(innerCirclePos.normalized);
+                    }
+                }
+                FillCircleSectionsWithTriangles(indices, indexOffset, circleSubdiv, nbSectionsOnCorner);
+                indexOffset += nbCirclesOnCorner * circleSubdiv;
+            }
+            #endregion
+
+            #region bottom-left-corner: outer-circle from -X(left) to -Y(bottom), circle right axis = +Z
+            {
+                Vector3 cornerCenter = panelBottomLeft + new Vector3(+margin, +margin, 0);
+                Vector3 unitRight = new Vector3(0, 0, 1);
+                Vector3 unitUp = new Vector3(0, 1, 0);
+                for (int cs = 0; cs < nbCirclesOnCorner; ++cs)
+                {
+                    float fQuarterPercent = 0.25f * (float)cs / (float)(nbCirclesOnCorner - 1); // [0..1], from top to left of corner quarter of circle.
+                    Vector3 outerCirclePos = new Vector3(
+                        -margin * Mathf.Cos(2.0f * Mathf.PI * fQuarterPercent),
+                        -margin * Mathf.Sin(2.0f * Mathf.PI * fQuarterPercent),
+                        0);
+                    Vector3 innerCircleCenter = cornerCenter + outerCirclePos;
+                    unitUp = -outerCirclePos.normalized;
+                    for (int i = 0; i < circleSubdiv; ++i)
+                    {
+                        float fi = (float)i / (float)circleSubdiv;
+
+                        Vector3 innerCirclePos = unitRight * radius * Mathf.Cos(2.0f * Mathf.PI * fi)
+                                                + unitUp * radius * Mathf.Sin(2.0f * Mathf.PI * fi);
+                        vertices.Add(innerCircleCenter + innerCirclePos);
+
+                        normals.Add(innerCirclePos.normalized);
+                    }
+                }
+                FillCircleSectionsWithTriangles(indices, indexOffset, circleSubdiv, nbSectionsOnCorner);
+                indexOffset += nbCirclesOnCorner * circleSubdiv;
+            }
+            #endregion
+
+            Mesh mesh = new Mesh();
+            mesh.SetVertices(vertices);
+            mesh.SetNormals(normals);
+            mesh.SetIndices(indices, MeshTopology.Triangles, 0);
+
+            return mesh;
+        }
+
+        private static void FillCircleSectionsWithTriangles(List<int> indices, int indexOffset, int circleSubdiv, int nbSections)
+        {
+            for (int cs = 0; cs < nbSections; ++cs)
+            {
+                for (int i = 0; i < circleSubdiv - 1; ++i)
+                {
+                    int idx0 = cs * circleSubdiv + i;
+                    int idx1 = (cs + 1) * circleSubdiv + i;
+                    int idx2 = (cs + 1) * circleSubdiv + i + 1;
+                    int idx3 = cs * circleSubdiv + i + 1;
+                    indices.Add(indexOffset + idx0);
+                    indices.Add(indexOffset + idx1);
+                    indices.Add(indexOffset + idx3);
+                    indices.Add(indexOffset + idx3);
+                    indices.Add(indexOffset + idx1);
+                    indices.Add(indexOffset + idx2);
+                }
+                {   // loop to first vertices in circles
+                    int idx0 = (cs * circleSubdiv + (circleSubdiv - 1));
+                    int idx1 = ((cs + 1) * circleSubdiv + (circleSubdiv - 1));
+                    int idx2 = ((cs + 1) * circleSubdiv + 0);
+                    int idx3 = (cs * circleSubdiv + 0);
+                    indices.Add(indexOffset + idx0);
+                    indices.Add(indexOffset + idx1);
+                    indices.Add(indexOffset + idx3);
+                    indices.Add(indexOffset + idx3);
+                    indices.Add(indexOffset + idx1);
+                    indices.Add(indexOffset + idx2);
+                }
+            }
+        }
+        
         #endregion
 
         #region Resource Loading
