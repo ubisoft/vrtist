416f064736c47a7dd727dc5e146c9ffac6249b4c
ewik <ewik@free.fr>
2021-01-19 10:47:03 +0100

    wip: ci/cd

--- .gitlab/build.bat
+++ .gitlab/build.bat
@@ -1,6 +1,6 @@
 REM @ECHO OFF
 REM Build Unity from a builder script
-%0 -quit -batchmode -projectPath %~dp1..\ -executeMethod VRtist.Builder.PerformBuild -logFile - %*
+%1 -quit -batchmode -projectPath %~dp0..\ -executeMethod VRtist.Builder.PerformBuild -logFile - %2 %3
 
 REM Build unity from the default build settings
 REM %0 -quit -batchmode -buildWindows64Player "Build/VRtist.exe" -logFile "Build/build.log"
