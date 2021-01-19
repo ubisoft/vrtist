76e3205046f0812d69317c64386e4ddbd624f39e
ewik <ewik@free.fr>
2021-01-19 10:17:55 +0100

    wip: ci/cd

--- .gitlab/build.bat
+++ .gitlab/build.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 REM Build Unity from a builder script
-"C:\Program Files\Unity\Editor\2019.3.1f1\Editor\Unity.exe" -quit -batchmode -projectPath %~dp0..\ -executeMethod VRtist.Builder.PerformBuild -logFile - %*
+%0 -quit -batchmode -projectPath %~dp1..\ -executeMethod VRtist.Builder.PerformBuild -logFile - %*
 
 REM Build unity from the default build settings
-REM "C:\Program Files\Unity\Editor\2019.3.1f1\Editor\Unity.exe" -quit -batchmode -buildWindows64Player "Build/VRtist.exe" -logFile "Build/build.log"
+REM %0 -quit -batchmode -buildWindows64Player "Build/VRtist.exe" -logFile "Build/build.log"
