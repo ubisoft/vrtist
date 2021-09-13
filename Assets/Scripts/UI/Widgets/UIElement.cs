1911c5714b4e6577cdc21422f9385bea12da5d38
Anthony Mirabile <amirabil@ad.inria.fr>
2021-09-13 19:16:32 +0200

    Haptics restored

--- Assets/Scripts/UI/Widgets/UIElement.cs
+++ Assets/Scripts/UI/Widgets/UIElement.cs
@@ -170,12 +170,12 @@ namespace VRtist
 
         public static void WidgetBorderHapticFeedback()
         {
-            VRInput.SendHaptic(VRInput.primaryController, 0.005f, 0.004f);
+            VRInput.SendHaptic(VRInput.primaryController, 0.025f, 0.1f);
         }
 
         public static void ClickHapticFeedback()
         {
-            VRInput.SendHaptic(VRInput.primaryController, 0.01f, 0.05f);
+            VRInput.SendHaptic(VRInput.primaryController, 0.05f, 0.3f);
         }
 
         #region ray
